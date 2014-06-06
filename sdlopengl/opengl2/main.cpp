f75a52a0312e5333347bd18c8a054901492c24dc
zhonghua <secondsquare@gmail.com>
2014-06-06 02:56:11 -0700

    color

--- sdlopengl/opengl2/main.cpp
+++ sdlopengl/opengl2/main.cpp
@@ -173,12 +173,16 @@ void render()
 	
 	glTranslatef(-1.5f, 0.0f, -6.0f);
 	glBegin(GL_TRIANGLES);
+    glColor3f(1.0f,0.0f,0.0f);
 		glVertex3f(0.0f, 1.0f, depth);
+    glColor3f(0.0f,1.0f,0.0f);
 		glVertex3f(-1.0f, -1.0f, depth);
+    glColor3f(0.0f,0.0f,1.0f);
 		glVertex3f(1.0f, -1.0f, depth);
 	glEnd();
 	
 	glTranslatef(3.0f, 0.0f, 0.0f);
+  glColor3f(0.5f,0.5f,1.0f);
 	glBegin(GL_QUADS);
 		glVertex3f(-1.0f, 1.0f, depth);
 		glVertex3f(1.0f, 1.0f, depth);
